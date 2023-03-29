68c93e4bb7de313d47b633a786a41f4422d4e39d
konteh <kontehebrima993@gmail.com>
2023-03-29 12:20:34 +0000

    complete the transaction screen

--- Sms-Inventory/screens/transaction/transaction.jsx
+++ Sms-Inventory/screens/transaction/transaction.jsx
@@ -11,15 +11,68 @@ import {
   import { AntDesign, Ionicons } from "@expo/vector-icons";
   import CustomSearch from "../../components/CustomSearch";
   import CustomCard from "../../components/CustomCard";
+  import { Feather } from '@expo/vector-icons';
 
   
   const TransactionScreen = () => {
     return(
-        <View>
-            <Text>Transaction Screen</Text>
+        <View style={styles.conatinter}>
+        <View style={styles.top}>
+          <View style={styles.flexContainer}>
+            <TouchableOpacity style={styles.left}>
+              <AntDesign name="left" size={24} color="#fff" />
+              <CustomText color="#fff">All Transaction</CustomText>
+            </TouchableOpacity>
+  
+            <View style={styles.right}>
+              {/* <TouchableOpacity style={{ right: 10 }}>
+              <AntDesign name="filter" size={24} color="#fff" />
+              </TouchableOpacity> */}
+              <TouchableOpacity style={{ left: 2 }}>
+              <AntDesign name="filter" size={24} color="#fff" />
+              </TouchableOpacity>
+              <TouchableOpacity style={{ left: 10 }}>
+              <Feather name="more-vertical" size={24} color="#fff" />
+              </TouchableOpacity>
+            </View>
+          </View>
+          {/* <View style={{ marginTop: 35 }}>
+            <CustomSearch mx={10} />
+          </View> */}
         </View>
+  
+        <CustomCard bc="black" mt={-30} mx={10}>
+          {/* <CustomText>Total Product:</CustomText> */}
+          
+          <CustomText style={{ marginTop: 5 }}>630 In:</CustomText>
+          <CustomText style={{ marginTop: 5 }}>630 Out:</CustomText>
+          <CustomText style={{ marginTop: 5 }}>630 In-hand:</CustomText>
+        </CustomCard>
+        
+       
+      </View>
     )
   }
 
   export default TransactionScreen;
-  const styles = StyleSheet.create({});
+
+  const styles = StyleSheet.create({
+    conatinter: {},
+    top: {
+      height: 150,
+      backgroundColor: COLORS.green,
+    },
+    right: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    left: {
+      flexDirection: "row",
+    },
+    flexContainer: {
+      flexDirection: "row",
+      marginHorizontal: 15,
+      marginTop: 50,
+      justifyContent: "space-between",
+    },
+  });
