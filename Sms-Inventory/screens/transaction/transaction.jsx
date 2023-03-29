ae6cca80a0d7fff4220b44d9678542621ac874c1
konteh <kontehebrima993@gmail.com>
2023-03-29 11:59:13 +0000

    create the transaction code base

---
+++ Sms-Inventory/screens/transaction/transaction.jsx
@@ -0,0 +1,25 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    AppRegistry,
+  } from "react-native";
+  import React from "react";
+  import { COLORS } from "../../constants/Theme";
+  import CustomText from "../../components/CustomText";
+  import { AntDesign, Ionicons } from "@expo/vector-icons";
+  import CustomSearch from "../../components/CustomSearch";
+  import CustomCard from "../../components/CustomCard";
+
+  
+  const TransactionScreen = () => {
+    return(
+        <View>
+            <Text>Transaction Screen</Text>
+        </View>
+    )
+  }
+
+  export default TransactionScreen;
+  const styles = StyleSheet.create({});
