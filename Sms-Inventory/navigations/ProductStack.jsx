51d4405644a6f529239648360a349aee8406a3b2
konteh <kontehebrima993@gmail.com>
2023-04-12 19:06:56 +0000

    add low product screen

--- Sms-Inventory/navigations/ProductStack.jsx
+++ Sms-Inventory/navigations/ProductStack.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import ProductScreen from "../screens/products";
 import { COLORS } from "../constants/Theme";
+import Addproduct from "../screens/products/addProduct";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const ProductStack = () => {
       <StatusBar backgroundColor={COLORS.green} style="light" />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Product" component={ProductScreen} />
+        <Stack.Screen name="add" component={Addproduct} />
       </Stack.Navigator>
     </>
   );
---
+++ Sms-Inventory/screens/products/addProduct.jsx
@@ -0,0 +1,19 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    AppRegistry,
+  } from "react-native";
+  import React from "react";
+
+
+  const Addproduct = () =>{
+    return(
+        <View>
+            <Text>Add product</Text>
+        </View>
+    )
+  }
+
+  export default Addproduct;
