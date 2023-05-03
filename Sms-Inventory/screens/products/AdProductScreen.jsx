2317529a1c00ab5da55686e9b311be38c1c403c3
konteh <kontehebrima993@gmail.com>
2023-05-03 14:26:41 +0000

    added a product add screen

---
+++ Sms-Inventory/screens/products/AdProductScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AdProductScreen = () => {
+  return (
+    <View style={{marginTop: 50}} >
+      <Text>OnboardingScreen</Text>
+    </View>
+  );
+};
+
+export default AdProductScreen;
+
+const styles = StyleSheet.create({});
