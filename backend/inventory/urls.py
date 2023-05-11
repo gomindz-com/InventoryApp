81311a4ffa8edcd3c3affa125c2568cacde1e584
modoucolley <mbasirucolley@gmail.com>
2023-05-11 12:37:04 +0000

    Updated The URLs and Views For Customer Management

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -8,10 +8,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', Welcome, name='welcome'),
+    path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/user/', include(('users.urls', 'users'), namespace='users')),
+    path('api/customer/', include(('users.urls', 'users'), namespace='users')),
     path('api/store/', include(('store.urls', 'store'), namespace='store')),
 
 ]
