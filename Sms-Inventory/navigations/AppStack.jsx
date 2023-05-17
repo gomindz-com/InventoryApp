1a378b43e36074a891251e9bb57b47199d0254c1
konteh <kontehebrima993@gmail.com>
2023-05-17 09:51:11 +0000

    app stack

--- Sms-Inventory/navigations/AppStack.jsx
+++ Sms-Inventory/navigations/AppStack.jsx
@@ -13,7 +13,7 @@ const Tab = createBottomTabNavigator();
 const AppStack = () => {
   return (
     <>
-      <Tab.Navigator
+      <Tab.Navigator 
         screenOptions={{
           headerShown: false,
           tabBarStyle: {
