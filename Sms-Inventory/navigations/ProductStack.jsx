1967e0af1103c43c13c210bb86027f02427faaf1
konteh <kontehebrima993@gmail.com>
2023-05-17 15:45:11 +0000

    productstack fix

---
+++ Sms-Inventory/navigations/ProductStack.jsx
@@ -0,0 +1,22 @@
+import { useTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import ProductScreen from "../screens/products";
+import { COLORS } from "../constants/Theme";
+import AdProductScreen from "../screens/products/AdProductScreen";
+
+const Stack = createNativeStackNavigator();
+
+const ProductStack = () => {
+  return (
+    <>
+      <StatusBar backgroundColor={COLORS.green} style="light" />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Product" component={ProductScreen} />
+        <Stack.Screen name="addProduct" component={AdProductScreen} />
+      </Stack.Navigator>
+    </>
+  );
+};
+
+export default ProductStack;
