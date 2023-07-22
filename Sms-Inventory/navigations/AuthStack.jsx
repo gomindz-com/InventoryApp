2197ba871ee88dd4b88dfdb5e4712ec3d0d55116
Muha~Boy <mn22011047@utg.edu.gm>
2023-07-22 20:11:25 +0000

    login and registration

--- Sms-Inventory/navigations/AuthStack.jsx
+++ Sms-Inventory/navigations/AuthStack.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import OnboardingScreen from "../screens/onboarding/OnboardingScreen";
+// import OnboardingScreen from "../screens/onboarding/OnboardingScreen";
 import SplashScreen from "../screens/auth/SplashScreen";
 import LoginScreen from "../screens/auth/LoginScreen";
 import RegisterScreen from "../screens/auth/RegisterScreen";
+import ForgotPassword from "../screens/auth/ForgotPassword";
+import ResetPassword  from "../screens/auth/Resetpassword";
 import AppStack from "./AppStack";
 
 const Stack = createNativeStackNavigator();
@@ -12,10 +14,12 @@ const AuthStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {/* <Stack.Screen component={OnboardingScreen} name="Onboarding" /> */}
-      {/* <Stack.Screen component={SplashScreen} name="Splash" /> */}
+      <Stack.Screen component={SplashScreen} name="Splash" />
       <Stack.Screen component={LoginScreen} name="Login" />
       <Stack.Screen component={AppStack} name="Main" />
       <Stack.Screen component={RegisterScreen} name="Register" />
+      <Stack.Screen component={ForgotPassword} name="Forgot" />
+      <Stack.Screen component={ResetPassword} name="Reset" />
     </Stack.Navigator>
   );
 };
--- Sms-Inventory/screens/auth/SplashScreen.jsx
+++ Sms-Inventory/screens/auth/SplashScreen.jsx
@@ -1,15 +1,109 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import * as Animatable from 'react-native-animatable';
+import { AntDesign } from '@expo/vector-icons';
+
 
 const SplashScreen = () => {
+  const navigation = useNavigation();
+
+  const HomeScreen = () => {
+    navigation.navigate("Login");
+  };
 
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Animatable.Image
+        animation="bounceIn"
+          source={require("../../../Sms-Inventory/assets/logo.png")}
+          style={styles.logo}
+          resizeMode="stretch"
+        />
+      </View>
+      <Animatable.View 
+      animation="fadeInUpBig"
+      style={styles.footer}>
+        <Text style={styles.title}>Stay connected with everyone!</Text>
+        <Text style={styles.text}>Sign in with account</Text>
+        <View style={styles.button}>
+        <TouchableOpacity style={styles.signIn} onPress={HomeScreen}>
+            <Text style={styles.textSign}>Get Started</Text>
+            <AntDesign name="right" size={15} color="#fff" style={{left: 3}} />
+        </TouchableOpacity>
+        </View>
+      </Animatable.View>
     </View>
-  )
-}
+  );
+};
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({})
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#337037",
+  },
+
+  header: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  footer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+
+  logo: {
+    width: height_logo,
+    height: height_logo,
+  },
+
+  title: {
+    color: '#05375a',
+    fontSize: 30,
+    fontWeight: 'bold'
+},
+text: {
+    color: "#565656",
+    marginTop:5,
+    fontSize: 15
+},
+
+button: {
+    alignItems: 'flex-end',
+    marginTop: 30
+},
+signIn: {
+    width: 150,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    flexDirection: 'row',
+    backgroundColor: "#337037"
+},
+    
+textSign: {
+    color: 'white',
+    fontWeight: 'bold',
+    right: 5
+}
+});
