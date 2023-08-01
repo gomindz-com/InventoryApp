1c66fdac28ea48f69e4168b7a07503d80c0f10d4
ModouLaminJagne <jagnemlg@gmai.com>
2023-08-01 13:43:53 +0000

    remove comments from URLs

--- backend/mobile/urls.py
+++ backend/mobile/urls.py
@@ -27,24 +27,7 @@ urlpatterns = [
      path('stats', StoreStatisticsView.as_view(), name='statsView'),
 
 
-     path('lowstockproduct/', lowstockproduct),
-     # path('totalstockIn/', total_stock_in),
-     # path('totalstockOut/', total_stock_out),
-     # path('totalstockInHand/', total_stock_in_hand),
+     path('lowstockproduct/', lowstockproduct)
 
-     
-
-
-
-    
-     # path('orders/<int:pk>', OrderRetreiveUpdateDeleteView.as_view(), name='detaildeleteorders'),
-     # path('storestatistics', StoreStatisticsView.as_view(), name='liststorestatistics'),
-    
-#     path('ordercount/', orderCounts),
-#     path('productcount/', productCounts),
-#     path('totalstock/', total_stock),
-#     path('totalprice/', total_price),
-#     path('cashpending/', cash_invoice),
-#     path('cashpending/<int:id>', cash_invoice),
 
 ]
