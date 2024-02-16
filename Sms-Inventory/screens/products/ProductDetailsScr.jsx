27d6db0192ca1da4ab60a1050239d1a289597534
konteh <kontehebrima993@gmail.com>
2024-02-16 11:21:33 +0000

    done with product detail styling

--- Sms-Inventory/screens/products/ProductDetailsScr.jsx
+++ Sms-Inventory/screens/products/ProductDetailsScr.jsx
@@ -5,6 +5,7 @@ import CustomText from "../../components/CustomText";
 import CustomInput from "../../components/CustomInput";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import CustomButton from "../../components/CustomButton";
 
 const ProductDetailsScreen = ({ route }) => {
   const { item } = route.params;
@@ -38,10 +39,26 @@ const ProductDetailsScreen = ({ route }) => {
           <CustomInput mx={10} mt={10} value={item.id.toString()} />
         </View> */}
         <View style={styles.singleButton}>
-          <CustomText style={styles.h1}>Product:</CustomText>
+          <CustomText style={styles.h1}>Product ID:</CustomText>
           <CustomText style={styles.h4}>{item.id}</CustomText>
         </View>
-        <View style={{ marginTop: 10 }}>
+        <View style={styles.singleButton}>
+          <CustomText style={styles.h1}>Buy Rate:</CustomText>
+          <CustomText style={styles.h4}>{item.BuyRate}</CustomText>
+        </View>
+        <View style={styles.singleButton}>
+          <CustomText style={styles.h1}>Total In:</CustomText>
+          <CustomText style={styles.h4}>{item.BuyRate}</CustomText>
+        </View>
+        <View style={styles.singleButton}>
+          <CustomText style={styles.h1}>In Hand:</CustomText>
+          <CustomText style={styles.h4}>{item.BuyRate}</CustomText>
+        </View>
+        <View style={styles.singleButton}>
+          <CustomText style={styles.h1}>Stock Price:</CustomText>
+          <CustomText style={styles.h4}>{item.BuyRate}</CustomText>
+        </View>
+        {/* <View style={{ marginTop: 10 }}>
           <CustomText style={styles.itemField}>Buy Rate:</CustomText>
           <CustomInput mx={10} mt={10} value={item.BuyRate.toString()} />
         </View>
@@ -56,8 +73,58 @@ const ProductDetailsScreen = ({ route }) => {
         <View style={{ marginTop: 10 }}>
           <CustomText style={styles.itemField}>Stock Price:</CustomText>
           <CustomInput mx={10} mt={10} value={item.stockPrice.toString()} />
-        </View>
+        </View> */}
       </View>
+
+      {/* <View style={styles.buttonContainer}>
+        <CustomButton title={"Delete"} onPress={() => {}} />
+        <CustomButton title={"Edit"} onPress={() => {}} />
+        <CustomButton title={"Transactions"} onPress={() => {}} />
+      </View> */}
+
+
+      <View
+          style={{
+            flexDirection: "row",
+            marginTop: 60,
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginHorizontal: 25,
+          }}
+        >
+          <CustomButton
+            width={100}
+            height={45}
+            titleSize={12}
+            title={"Delete"}
+            br={15}
+            bg={"#fff"}
+            bc={"#2e9a90"}
+            // mx={10}
+          />
+
+          <CustomButton
+            br={15}
+            height={45}
+            titleSize={12}
+            width={100}
+            title={"Edit"}
+            bg={"#fff"}
+            bc={"#2e9a90"}
+            mx={10}
+          />
+          <CustomButton
+            br={15}
+            height={45}
+            titleSize={12}
+            width={120}
+            title={"Transactions"}
+            bg={"#fff"}
+            bc={"#2e9a90"}
+          />
+        </View>
+
+      
     </View>
   );
 };
@@ -91,12 +158,14 @@ const styles = StyleSheet.create({
   },
   singleButton: {
     flexDirection: "row",
-    width: "100%",
+    width: "95%",
     height: 60,
     elevation: 5,
     backgroundColor: "#f5faf8",
     borderRadius: 10,
     marginTop: 10,
+    marginLeft:10,
+    marginRight:100,
   },
   ProductDetailText: {
     justifyContent: "center",
@@ -112,10 +181,48 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 5,
   },
+  h1: {
+    alignSelf: "center",
+    color: "#2e9a90",
+    fontWeight: "bold",
+    fontSize: 20,
+    left: 10,
+  },
+  h4: {
+    marginLeft: 10,
+    alignSelf: "center",
+    color: "#2e9a90",
+    fontWeight: "bold",
+    fontSize: 20,
+    left: 10,
+  },
   itemField: {
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 60,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 25,
+  },
+  buttoncustom:{
+    alignSelf: "center",
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+    backgroundColor: "transparent", // No fill color
+      },
+
+       buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black", // Text color
   },
 });
 
